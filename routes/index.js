45c0fedda57bea2dc21a6ea13579bdd13b234889
wlsun0324 <wlsun0324@gmail.com>
2022-05-10 18:04:51 +0900

    Add : 로그인 부분 구현

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const router = express.Router();
 
 const testRouter = require('./testRoute');
+const userRouter = require('./userRouter');
 
 router.use('/test', testRouter);
+router.use('/user', userRouter);
 
 module.exports = router;
