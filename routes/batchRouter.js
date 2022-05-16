4af6c1ffa2f026d5075d9bc0b3ec596966dbcef3
wlsun0324 <wlsun0324@gmail.com>
2022-05-16 09:49:36 +0900

    Add : 출금 배치 중지 기능 추가

--- routes/batchRouter.js
+++ routes/batchRouter.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const withdrawalBatch = require("../batchs/withdrawalBatch");
 
 router.get("/withdrawal", withdrawalBatch.withdrawalBatch);
+router.get("/stop-withdrawal", withdrawalBatch.stopWithdrawalBatch);
 router.use("/", withdrawalBatch.error);
 
 module.exports = router;
